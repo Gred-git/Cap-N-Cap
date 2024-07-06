6f97a660e369ee344c40a8843edcfc4f8588e77d
Gred-git <90420916+Gred-git@users.noreply.github.com>
2024-07-06 06:25:07 +0000

    navbar setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import Navbar from './Navbar';
 
 function App() {
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsNavbarOpen(!isNavbarOpen);
+  };
+
+
   return (
     <div className="App">
 
-      <Navbar />
+      <button className="open-navbar-button" onClick={toggleNavbar}>
+        <div className="menu-icon"></div>
+        <div className="menu-icon"></div>
+        <div className="menu-icon"></div>
+      </button>
+      <Navbar isOpen={isNavbarOpen} toggleNavbar={toggleNavbar} />
     
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Cap N' Can
         </p>
         <a
           className="App-link"
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,25 +1,22 @@
-import React, { useState } from 'react';
+// Navbar.js
+
+import React from 'react';
 import './Navbar.css';
 
-const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleNavbar = () => {
-    setIsOpen(!isOpen);
-  };
-
+const Navbar = ({ isOpen, toggleNavbar }) => {
   return (
+
     <div className={`navbar ${isOpen ? 'open' : ''}`}>
       <button className="menu-button" onClick={toggleNavbar}>
-        Menu
       </button>
+    
       <nav className="navbar-menu">
+
         <ul>
+          <li><a href="#">About</a></li>
           <li><a href="#">Search</a></li>
           <li><a href="#">Challenges</a></li>
           <li><a href="#">Forums</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">~~~~~~</a></li>
         </ul>
       </nav>
     </div>
